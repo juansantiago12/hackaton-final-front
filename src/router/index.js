fccb83a7314b328e20ab3d618da695a353db2daf
Martin Rios <mar_1905@hotmail.es>
2021-07-29 23:17:08 -0500

    InfoCurso

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,38 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Carrito from '../views/Carrito.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Carrito from "../views/Carrito.vue";
 
 const routes = [
-   {
-      path: '/',
-      name: 'Home',
-      component: Home,
-   },
-   {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-   },
-   {
-      path: '/carrito',
-      name: 'Carrito',
-      component: Carrito,
-   },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/carrito",
+    name: "Carrito",
+    component: Carrito,
+  },
+  {
+    path: "/infocurso/:id",
+    name: "InfoCurso",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/InfoCurso.vue"),
+  },
 ];
 
 const router = createRouter({
-   history: createWebHashHistory(),
-   routes,
+  history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,48 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 
 export default createStore({
-   state: {
-      cursos: [],
-      cursoAgregado: [],
-      // cursoAgregadoFinal: [],
-   },
-   mutations: {
-      getCursosMutation(state, payload) {
-         state.cursos = payload;
-      },
-      addCursoMutation(state, payload) {
-         const element = state.cursos.filter((curso) => {
-            return curso.id === payload;
-         });
-         state.cursoAgregado.push(element[0]);
-         console.log(state.cursoAgregado);
-      },
-   },
-   actions: {
-      async getCursosAction({ commit }) {
-         const response = await fetch('https://no-llores-mas.herokuapp.com/cursos/');
-         const data = await response.json();
-         commit('getCursosMutation', data);
-      },
-      addCursoAction({ commit }, id) {
-         commit('addCursoMutation', id);
-      },
-   },
-   modules: {},
+  state: {
+    cursos: [],
+    cursoAgregado: [],
+    cursoInfo: [],
+    // cursoAgregadoFinal: [],
+  },
+  mutations: {
+    getCursosMutation(state, payload) {
+      state.cursos = payload;
+    },
+    addCursoMutation(state, payload) {
+      const element = state.cursos.filter((curso) => {
+        return curso.id === payload;
+      });
+      state.cursoAgregado.push(element[0]);
+      console.log(state.cursoAgregado);
+    },
+    /*etCursosInfoMutation(state, payload) {
+      state.cursoInfo = payload;
+
+      console.log(cursoInfo);
+    },*/
+  },
+  actions: {
+    async getCursosAction({ commit }) {
+      const response = await fetch(
+        "https://no-llores-mas.herokuapp.com/cursos/"
+      );
+      const data = await response.json();
+      commit("getCursosMutation", data);
+    },
+    addCursoAction({ commit }, id) {
+      commit("addCursoMutation", id);
+    },
+    /*async getCursosInfoAction({ commit }) {
+      const response = await fetch(
+        "https://no-llores-mas.herokuapp.com/cursos/7"
+      );
+      const data = await response.json();
+      commit("getCursosInfoMutation", data);
+      console.log(data);
+    },*/
+  },
+  modules: {},
 });
